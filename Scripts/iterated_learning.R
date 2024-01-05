7742bab3188a23acd0e9e88c95c259c9d94904b0
znhoughton <znhoughton@gmail.com>
2024-01-05 14:06:29 -0800

    New plots for genpref = 0.5

--- Scripts/iterated_learning.R
+++ Scripts/iterated_learning.R
@@ -1,4 +1,41 @@
-#two functions included here: iterated_learning which is a parallelized iterated learning model, and iterated_learning_not_parallelized which, unsurprisingly, is the original iterated learning model we created without parallelization.
+#three functions included here. First one is the main function. The other two are the iterated learning functions: iterated_learning which is a parallelized iterated learning model, and iterated_learning_not_parallelized which, unsurprisingly, is the original iterated learning model we created without parallelization.
+#details about the functions can be found in the noisy_channel_sims.Rmd file
+noisy_channel_learning_prod_noise = function(p_theta, N, prior_mu, nu, p_noise, prior_prob_noise) {
+  
+  
+  alpha_1 = prior_mu * nu        #from the beta distribution
+  alpha_2 = (1 - prior_mu) * nu  #from the beta distribution
+  
+  for (i in 1:N) {
+    generated_word = rbinom(n = 1, size = 1, prob = p_theta)
+    
+    prior_noise = rbinom(n = 1, size = 1, prob = 1-prior_prob_noise)
+    
+    if (generated_word == prior_noise) { #if we hear A and B
+      
+      un_normalized_p_hat_alpha = (alpha_1 / (alpha_1 + alpha_2)) * (1 - p_noise)
+      un_normalized_p_hat_nonalpha = (1 - (alpha_1 / (alpha_1 + alpha_2))) * p_noise
+      
+      
+      
+    }
+    
+    else { #if we hear B and A: it should be similar to above, but in this case, we actually did hear B and A, so we multiply it by p * noise, not 1 - p_noise
+      un_normalized_p_hat_alpha = (alpha_1 / (alpha_1 + alpha_2)) * p_noise #we mutiply this by 0.05 because we actually did hear B and A
+      un_normalized_p_hat_nonalpha = (1 - (alpha_1 / (alpha_1 + alpha_2))) *  (1 - p_noise) #multiply this by 1-p_noise because we heard correctly
+    }
+    
+    p_hat_alpha = un_normalized_p_hat_alpha / (un_normalized_p_hat_alpha + un_normalized_p_hat_nonalpha)
+    p_hat_nonalpha = 1 - (un_normalized_p_hat_alpha / (un_normalized_p_hat_alpha + un_normalized_p_hat_nonalpha))
+    
+    alpha_1 = alpha_1 + p_hat_alpha
+    alpha_2 = alpha_2 + p_hat_nonalpha
+  }
+  
+  return(alpha_1 / (alpha_1 + alpha_2))
+  #rbinom(p = alpha_1 / (alpha_1 + alpha_2))
+  
+}
 
 iterated_learning = function(n_gen, n_sims, p_theta, N, prior_mu, nu, p_noise, prior_prob_noise, last_gen_only = F) { #set last_gen_only to T if you wish to save memory and only care about the last generation.
   #mu_df = tibble(posterior_mu = numeric(), generation = numeric(), estimated_p_theta = numeric())
