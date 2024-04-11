fb1e1dd0583731ee98295914cd9bade6e8d94b5b
znhoughton <znhoughton@gmail.com>
2024-04-10 18:16:07 -0700

    revised sims

--- Scripts/iterated_learning.R
+++ Scripts/iterated_learning.R
@@ -7,16 +7,14 @@ noisy_channel_learning_prod_noise = function(p_theta, N, prior_mu, nu, p_noise, 
   alpha_2 = (1 - prior_mu) * nu  #from the beta distribution
   
   for (i in 1:N) {
-    generated_word = rbinom(n = 1, size = 1, prob = p_theta)
+    generated_word = rbinom(n = 1, size = 1, prob = p_theta) # 1 for AandB, 0 for BandA
     
-    prior_noise = rbinom(n = 1, size = 1, prob = 1-prior_prob_noise)
+    prior_noise = rbinom(n = 1, size = 1, prob = 1-prior_prob_noise) # 1 if speaker produces the correct utterance, 0 if not
     
     if (generated_word == prior_noise) { #if the speaker produces A and B and intends to produce A and B, or produces A and B but intends to produce B and A
       
       un_normalized_p_hat_alpha = (alpha_1 / (alpha_1 + alpha_2)) * (1 - p_noise)
       un_normalized_p_hat_nonalpha = (1 - (alpha_1 / (alpha_1 + alpha_2))) * p_noise
-      
-      
       
     }
     
@@ -96,4 +94,99 @@ iterated_learning_not_parallelized = function(n_gen, n_sims, p_theta, N, prior_m
   
   return(mu_df)
   
+}
+
+
+###testing what happens if they hear all AandB then BandA
+
+noisy_channel_learning_prod_noise_ordered = function(p_theta, N, prior_mu, nu, p_noise, prior_prob_noise) {
+  
+  
+  alpha_1 = prior_mu * nu        #from the beta distribution
+  alpha_2 = (1 - prior_mu) * nu  #from the beta distribution
+  
+  num_alpha = rbinom(n = 1, size = N, prob = p_theta)
+  num_nonalpha = N - num_alpha
+  
+  for (i in 1:num_alpha) {
+    
+    prior_noise = rbinom(n = 1, size = 1, prob = 1-prior_prob_noise)
+    
+    if (prior_noise == 1) {
+      un_normalized_p_hat_alpha = (alpha_1 / (alpha_1 + alpha_2)) * (1 - p_noise)
+      un_normalized_p_hat_nonalpha = (1 - (alpha_1 / (alpha_1 + alpha_2))) * p_noise
+      
+    }
+    
+    else { #if we hear B and A: it should be similar to above, but in this case, we actually did hear B and A, so we multiply it by p * noise, not 1 - p_noise
+      un_normalized_p_hat_alpha = (alpha_1 / (alpha_1 + alpha_2)) * p_noise #we mutiply this by 0.05 because we actually did hear B and A
+      un_normalized_p_hat_nonalpha = (1 - (alpha_1 / (alpha_1 + alpha_2))) *  (1 - p_noise) #multiply this by 1-p_noise because we heard correctly
+    }
+    
+    p_hat_alpha = un_normalized_p_hat_alpha / (un_normalized_p_hat_alpha + un_normalized_p_hat_nonalpha)
+    p_hat_nonalpha = 1 - (un_normalized_p_hat_alpha / (un_normalized_p_hat_alpha + un_normalized_p_hat_nonalpha))
+    
+    alpha_1 = alpha_1 + p_hat_alpha
+    alpha_2 = alpha_2 + p_hat_nonalpha
+  }
+  
+  for (i in 1:num_nonalpha) { #if the speaker intends to produce BandA
+    
+    prior_noise = rbinom(n = 1, size = 1, prob = 1-prior_prob_noise) #1 if speaker produces BandA
+    
+    if (prior_noise == 0) { #if they produce AandB
+      un_normalized_p_hat_alpha = (alpha_1 / (alpha_1 + alpha_2)) * (1 - p_noise)
+      un_normalized_p_hat_nonalpha = (1 - (alpha_1 / (alpha_1 + alpha_2))) * p_noise
+      
+    }
+    
+    else { #if we hear B and A: it should be similar to above, but in this case, we actually did hear B and A, so we multiply it by p * noise, not 1 - p_noise
+      un_normalized_p_hat_alpha = (alpha_1 / (alpha_1 + alpha_2)) * p_noise #we mutiply this by 0.05 because we actually did hear B and A
+      un_normalized_p_hat_nonalpha = (1 - (alpha_1 / (alpha_1 + alpha_2))) *  (1 - p_noise) #multiply this by 1-p_noise because we heard correctly
+    }
+    
+    p_hat_alpha = un_normalized_p_hat_alpha / (un_normalized_p_hat_alpha + un_normalized_p_hat_nonalpha)
+    p_hat_nonalpha = 1 - (un_normalized_p_hat_alpha / (un_normalized_p_hat_alpha + un_normalized_p_hat_nonalpha))
+    
+    alpha_1 = alpha_1 + p_hat_alpha
+    alpha_2 = alpha_2 + p_hat_nonalpha
+  }
+  
+  return(alpha_1 / (alpha_1 + alpha_2))
+  #rbinom(p = alpha_1 / (alpha_1 + alpha_2))
+  
+}
+
+
+
+iterated_learning_ordered = function(n_gen, n_sims, p_theta, N, prior_mu, nu, p_noise, prior_prob_noise, last_gen_only = F) { #set last_gen_only to T if you wish to save memory and only care about the last generation.
+  #mu_df = tibble(posterior_mu = numeric(), generation = numeric(), estimated_p_theta = numeric())
+  
+  sim_function = function() {
+    p_new_theta = p_theta
+    map_dfr(1:n_gen, ~{
+      #if (. %% 100 == 0) {
+      # print(sprintf('current generation is: %s', .))
+      #}
+      generation = .
+      posterior_mu = noisy_channel_learning_prod_noise_ordered(p_theta = p_new_theta, N = N, prior_mu = prior_mu, nu = nu, p_noise = p_noise, prior_prob_noise = prior_prob_noise)
+      
+      if(last_gen_only == F) {
+        result = tibble(posterior_mu = posterior_mu, generation = generation, estimated_p_theta = p_new_theta)
+        p_new_theta <<- posterior_mu #need to use the operator <<- to update p_new_theta on a global scope
+        
+        return(result)
+      }
+      else{
+        p_new_theta <<- posterior_mu #need to use the operator <<- to update p_new_theta on a global scope
+        if(generation == n_gen) {
+          result = tibble(posterior_mu = posterior_mu, generation = generation, estimated_p_theta = p_new_theta)
+        }
+      }
+    })
+  }
+  
+  mu_df <- future_map_dfr(1:n_sims, ~sim_function())
+  
+  return(mu_df)
 }
